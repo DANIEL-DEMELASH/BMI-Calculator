d39dec830fa383dd640aba9d500c50328982232b
DANIEL-DEMELASH <danieldemelash5@gmail.com>
2022-06-13 16:46:56 +0300

    build UI home page

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_container.dart';
+import 'reusable_icon_content.dart';
+import 'constants.dart';
+
+enum gender { male, female, nul }
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -8,71 +14,123 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = kInactiveContainerColor;
+  Color femaleColor = kInactiveContainerColor;
+  gender selectedGender = gender.nul;
+  int height = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 170.0,
-                  height: 200.0,
-                  margin: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFF1D1E33),
+        appBar: AppBar(
+          title: const Text('BMI CALCULATOR'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableContainer(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = gender.male;
+                      });
+                    },
+                    colour: (selectedGender == gender.male)
+                        ? kActiveContainerColor
+                        : kInactiveContainerColor,
+                    childWidget: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
+                    ),
+                  )),
+                  Expanded(
+                      child: ReusableContainer(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = gender.female;
+                      });
+                    },
+                    colour: (selectedGender == gender.female)
+                        ? kActiveContainerColor
+                        : kInactiveContainerColor,
+                    childWidget: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ReusableContainer(
+              onPress: () {},
+              colour: kActiveContainerColor,
+              childWidget: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: kTextStyle,
                   ),
-                ),
-                Container(
-                  width: 170.0,
-                  height: 200.0,
-                  margin: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFF1D1E33),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(
+                        height.toString(),
+                        style: kTextStyle2,
+                      ),
+                      Text(
+                        'cm',
+                        style: kTextStyle,
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  Slider(
+                      value: height.toDouble(),
+                      min: 120,
+                      max: 220,
+                      activeColor: Color(0xFFEB1515),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      })
+                ],
+              ),
+            )),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableContainer(
+                    onPress: () {},
+                    colour: kActiveContainerColor,
+                    childWidget: Container(),
+                  )),
+                  Expanded(
+                      child: ReusableContainer(
+                    onPress: () {},
+                    childWidget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[]),
+                    colour: kActiveContainerColor,
+                  )),
+                ],
+              ),
             ),
             Container(
-              width: 370.0,
-              height: 150.0,
-              margin: EdgeInsets.symmetric(vertical: 10.0),
-              decoration: BoxDecoration(
-                  color: const Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0)),
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 170.0,
-                  height: 200.0,
-                  margin: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFF1D1E33),
-                  ),
-                ),
-                Container(
-                  width: 170.0,
-                  height: 200.0,
-                  margin: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFF1D1E33),
-                  ),
-                ),
-              ],
-            ),
+              color: kBottomContainerColor,
+              margin: const EdgeInsets.only(top: 10.0),
+              height: kBottomContainerHeight,
+              width: double.infinity,
+            )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
